d4ac23abd828914551a7a6d7ab41956f35db1dc1
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-04-18 11:39:03 +0700

    Version 1.2.1

---
+++ WebsiteBanDTOnline/Migrations/202304180416174_Add-Migration Databasever1.0.cs
@@ -0,0 +1,18 @@
+﻿namespace WebsiteBanDTOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMigrationDatabasever10 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.tb_Orders", "Email");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.tb_Orders", "Email", c => c.String());
+        }
+    }
+}
---
+++ WebsiteBanDTOnline/Migrations/202304180426251_UpdateOrder.cs
@@ -0,0 +1,18 @@
+﻿namespace WebsiteBanDTOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateOrder : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Orders", "Email", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Orders", "Email");
+        }
+    }
+}
