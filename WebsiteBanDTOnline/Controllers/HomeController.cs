0dbb114ccfd226973b3d9595fbd98fcecb8cc71b
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-04-19 21:41:25 +0700

    Add LogIn and LogOut(User)

--- WebsiteBanDTOnline/Controllers/HomeController.cs
+++ WebsiteBanDTOnline/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using WebsiteBanDTOnline.Models;
 using WebsiteBanDTOnline.Models.EF;
 
