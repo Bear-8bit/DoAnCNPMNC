05a871a4ab62aeb7b1f591391db30bcb50d068b4
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-04-19 22:35:31 +0700

    Update Database

---
+++ WebsiteBanDTOnline/Migrations/202304191534492_Users.cs
@@ -0,0 +1,29 @@
+﻿namespace WebsiteBanDTOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Users : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.tb_User");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.tb_User",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirsName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
