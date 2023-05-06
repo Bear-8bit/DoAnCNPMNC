fc5ddecdb37d425d6df6c9ada48a66ff994fca5b
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-05-07 02:41:29 +0700

    Fix-Navbar

--- WebsiteBanDTOnline/Controllers/ProductsController.cs
+++ WebsiteBanDTOnline/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using WebsiteBanDTOnline.Models;
 
 namespace WebsiteBanDTOnline.Controllers
@@ -58,5 +59,6 @@ namespace WebsiteBanDTOnline.Controllers
             var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(12).ToList();
             return PartialView(items);
         }
+  
     }
 }
